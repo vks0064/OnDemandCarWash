a7d7563bb640da44d52fd04aa4ae7dbcd26c1f93
vkumars7 <vkumars7@lin33004824.corp.capgemini.com>
2021-12-07 13:55:39 +0530

    fifth commit

---
+++ washer/src/main/java/com/example/demo/WasherApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasherApplication.class, args);
+	}
+
+}
